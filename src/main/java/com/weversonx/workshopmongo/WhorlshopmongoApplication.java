cd77916b7533f632f73e4726fcbb7094edebbf66
Weversonx <wevercb11@gmail.com>
2023-05-17 20:13:15 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/weversonx/workshopmongo/WhorlshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.weversonx.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorlshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhorlshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weversonx/workshopmongo/WhorlshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weversonx.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorlshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
